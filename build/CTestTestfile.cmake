bfa820371fac92b23e4642014cf0f3727b30b45c
Yalun Wen <ywen014@gmail.com>
2020-01-17 10:36:17 -0800

    edited in

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/ywen/yalun_test/src
+# Build directory: /home/ywen/yalun_test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(joint_state_publisher/joint_state_publisher_gui)
+subdirs(joint_state_publisher/joint_state_publisher)
---
+++ build/joint_state_publisher/joint_state_publisher_gui/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ywen/yalun_test/src/joint_state_publisher/joint_state_publisher_gui
+# Build directory: /home/ywen/yalun_test/build/joint_state_publisher/joint_state_publisher_gui
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
